f6914ed6cb4d976c6a4faa831855a1bb109f91bd
utterliar1 <wlup@github.com>
2021-05-30 13:18:48 +0800

    update

--- backup/diy_scripts.sh
+++ backup/diy_scripts.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+## 编辑docker-compose.yml文件添加 - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/mixool/jd_sku/main/jd_diy.sh
+### CUSTOM_SHELL_FILE for https://gitee.com/lxk0301/jd_docker/tree/master/docker
+#### DIY脚本仅供参考,由于更新可能引入未知BUG,建议Fork后使用自己项目的raw地址
+
+function monkcoder(){
+    # https://github.com/monk-coder/dust
+    rm -rf /monkcoder /scripts/monkcoder_*
+    git clone https://github.com/utterliar1/monkcoder.git /monkcoder
+    for jsname in $(find /monkcoder -name "*.js" | grep -vE "\/backup\/|adolf_pk.js|adolf_star.js|z_health_energy.js|z_city_cash.js|z_carnivalcity.js|z_mother_jump.js|z_xmf.js|z_health_community.js|monk_shop_follow_sku.js|monk_shop_add_to_car.js"); do cp ${jsname} /scripts/monkcoder_${jsname##*/}; done
+}
 
 function nianyuguai(){
     # https://github.com/nianyuguai/longzhuzhu.git
@@ -24,13 +34,13 @@ function didi_fruit(){
 function utterliar(){
     # https://github.com/utterliar1/Dockerfile.git
     rm -rf /utterliar /scripts/utterliar_*
-    git clone -b master https://github.com/utterliar1/Dockerfile.git /utterliar
-    for jsname in $(find /utterliar -name "*.js" | grep -vE "\/backup\/"); do cp ${jsname} /scripts/utterliar_${jsname##*/}; done
+    git clone -b main https://github.com/utterliar1/Dockerfile.git /utterliar
+    for jsname in $(ls /utterliar | grep -oE ".*\js$"); do cp -rf /utterliar/$jsname /scripts/utterliar_$jsname; done
 }
 
 function diycron(){
-    # jddj didi_fruit utterliar定时任务
-    for jsname in /scripts/utterliar_*.js /scripts/jddj_*.js /scripts/didi_fruit_*.js; do
+    # monkcoder jddj didi_fruit定时任务
+    for jsname in /scripts/monkcoder_*.js /scripts/jddj_*.js /scripts/didi_fruit_*.js; do
         jsnamecron="$(cat $jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
         test -z "$jsnamecron" || echo "$jsnamecron node $jsname >> /scripts/logs/$(echo $jsname | cut -d/ -f3).log 2>&1" >> /scripts/docker/merged_list_file.sh
     done
@@ -45,6 +55,7 @@ function main(){
     # DIY脚本
     a_jsnum=$(ls -l /scripts | grep -oE "^-.*js$" | wc -l)
     a_jsname=$(ls -l /scripts | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    monkcoder
     nianyuguai
     jddj
     didi_fruit
